bb1447d728925b78208f8bd27dc7b0a9523f0415
viisualworks <dshege4@gmail.com>
2024-09-13 14:54:35 +0300

    add node modules to gitignore file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: "Manrope, system-ui"
+      },
+      colors: {
+        darkGrayishBlue: "hsl(217, 19%, 35%)",
+        darkBlue: "hsl(214, 17%, 51%)",
+        grayishBlue: "hsl(212, 23%, 69%)",
+        lightGrayishBlue: "hsl(210, 46%, 95%)"
+      },
+      fontSize: {
+        base: "13px",
+      }
+    },
   },
   plugins: [],
 }
